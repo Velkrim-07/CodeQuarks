78686c385d30186477f0f561b05b0ea4d32b50d5
Velkrim-07 <burnin311@gmail.com>
2023-11-02 14:08:00 -0400

    Things are broken but thats ok. Currently the app will not load and I don't know why. I have added all of the required objects to add in a navigation bar and it ended up flipping on me. Getting it back up and running is the most important thing now

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,6 +54,7 @@ dependencies {
     implementation("androidx.navigation:navigation-fragment-ktx:2.5.3")
     implementation("androidx.navigation:navigation-ui-ktx:2.5.3")
     implementation("com.google.firebase:firebase-database:20.3.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/com/example/codequarks/MainActivity.kt
+++ app/src/main/java/com/example/codequarks/MainActivity.kt
@@ -5,18 +5,22 @@ import android.content.Intent
 import android.graphics.Bitmap
 import android.os.Bundle
 import android.provider.MediaStore
+import android.view.MenuItem
 import android.view.View
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.codequarks.databinding.ActivityMainBinding
+import com.google.android.material.navigation.NavigationView
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.ValueEventListener
@@ -30,6 +34,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     lateinit var cameraButton: Button
     lateinit var ImageView: ImageView
+    lateinit var toggle : ActionBarDrawerToggle
     val REQUEST_IMAGE_CAPTURE = 100
     val database = Firebase.database //Database connection
 
@@ -39,7 +44,7 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val navView: BottomNavigationView = binding.navView
+        val navView: BottomNavigationView = binding.bottomNavView
 
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
         // Passing each menu ID as a set of Ids because each
@@ -73,6 +78,24 @@ class MainActivity : AppCompatActivity() {
             })
         }
 
+        val drawerLayout : DrawerLayout = findViewById(R.id.drawerLayout)
+        val leftNavView : NavigationView = findViewById(R.id.left_nav_view)
+
+        toggle = ActionBarDrawerToggle(this,drawerLayout,R.string.open,R.string.close)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        leftNavView.setNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.nav_home -> Toast.makeText(applicationContext, "Clicked Home", Toast.LENGTH_SHORT).show()
+                R.id.nav_info -> Toast.makeText(applicationContext, "Clicked Info", Toast.LENGTH_SHORT).show()
+                R.id.nav_quiz -> Toast.makeText(applicationContext, "Clicked Quiz", Toast.LENGTH_SHORT).show()
+                R.id.nav_settings -> Toast.makeText(applicationContext, "Clicked settings", Toast.LENGTH_SHORT).show()
+            }
+            true
+        }
 
         ImageView = findViewById(R.id.imageSave) //Grabs the image view with the id
         cameraButton = findViewById(R.id.CameraButton) //Button for taking a picture
@@ -123,5 +146,14 @@ class MainActivity : AppCompatActivity() {
         Button3.text = "EhLLo"
         Button4.text = "DLrow"
 
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        if (toggle.onOptionsItemSelected(item)){
+            return true
+        }
+
+        return super.onOptionsItemSelected(item)
     }
 }
