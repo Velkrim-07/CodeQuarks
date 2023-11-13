8f8ea105873b00e9a9d12a18fcd9b3d29d3c9692
Velkrim-07 <burnin311@gmail.com>
2023-11-12 23:45:10 -0500

    Mocked everything up so that its "functional". Additionally The camera is a little fucky and sometimes wont work

--- app/src/main/java/com/example/codequarks/MainActivity.kt
+++ app/src/main/java/com/example/codequarks/MainActivity.kt
@@ -31,7 +31,7 @@ import com.google.firebase.ktx.Firebase
 import kotlin.concurrent.fixedRateTimer
 import kotlin.random.Random
 
-class MainActivity : AppCompatActivity(){
+class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     lateinit var cameraButton: Button
@@ -62,20 +62,29 @@ class MainActivity : AppCompatActivity(){
 
 
         //Button, Textview, and Database declaration
-        displayButton = findViewById(R.id.menu_Description_Button) //Button to display the data in the database
+        displayButton =
+            findViewById(R.id.menu_Description_Button) //Button to display the data in the database
         //TextView to show the entry in the database
 
-        val myList = listOf("Variable", "Classes", "Functions", "Overview", "message") //List of all the keys for the database entries
-        val randomIndex = Random.nextInt(myList.size) //random index of the array to apply randomness
+        val myList = listOf(
+            "Variable",
+            "Classes",
+            "Functions",
+            "Overview",
+            "message"
+        ) //List of all the keys for the database entries
+        val randomIndex =
+            Random.nextInt(myList.size) //random index of the array to apply randomness
         randomElement = myList[randomIndex] //Get the random key from the array
-        val myRef = database.getReference(randomElement).child("Desc")//get the description of the entry from the database
+        val myRef = database.getReference(randomElement)
+            .child("Desc")//get the description of the entry from the database
 
         //ImageView = findViewById(R.id.imageSave) //Grabs the image view with the id
         cameraButton = findViewById(R.id.menu_Camera_Button) //Button for taking a picture
 
         cameraButton.setOnClickListener {//When the button is clicked initiate the camera
             setContentView(R.layout.fragment_camera)
-           /* val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            /* val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
             try {
                 startActivityForResult(takePictureIntent,REQUEST_IMAGE_CAPTURE)
             } catch (e: ActivityNotFoundException){
@@ -85,17 +94,18 @@ class MainActivity : AppCompatActivity(){
 
     }
 
-   /* override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK){
-            val imageBitmap = data?.extras?.get("data") as Bitmap //save the data as a bitmap to be plotted onto the image view
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            val imageBitmap =
+                data?.extras?.get("data") as Bitmap //save the data as a bitmap to be plotted onto the image view
             ImageView.setImageBitmap(imageBitmap) //Plot the bitmap on the image view
         } else {
             super.onActivityResult(requestCode, resultCode, data)
         }
-    }*/
+    }
 
     fun goToDesc(view: View) {
-        var navController : NavController
+        /*var navController : NavController
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.navigation_home) as? NavHostFragment
         navHostFragment?.let{navHostFragment ->
             navController = NavHostFragment.findNavController(navHostFragment)
@@ -109,15 +119,67 @@ class MainActivity : AppCompatActivity(){
             }
         } ?: run {
 
-        }
+        }*/
+        setContentView(R.layout.fragment_desc)
+        val DescTextView: TextView = findViewById(R.id.Desc)
+        var DescRef = database.getReference("message").child("Desc")
+        DescRef.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val value = snapshot.getValue<String>()
+                DescTextView.text =
+                    value //sets the text view so the description is displayed to the user
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                DescTextView.text = "Failed to load data"
+            }
+        })
     }
 
     fun goToQuiz(view: View) {
         setContentView(R.layout.fragment_quiz)
+        val quizTextView: TextView = findViewById(R.id.Prompt)
+        val quizRef = database.getReference("message").child("Quiz").child("Prompt")
+        quizRef.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val value = snapshot.getValue<String>()
+                quizTextView.text = value //sets the text view so the quiz prompt
+
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                quizTextView.text = "Failed to load data"
+            }
+        })
+
+        val Button1: Button = findViewById(R.id.Choice1)
+        val Button2: Button = findViewById(R.id.Choice2)
+        val Button3: Button = findViewById(R.id.Choice3)
+        val Button4: Button = findViewById(R.id.Choice4)
+        Button1.text = "Correct"
+        Button2.text = "Wrong"
+        Button3.text = "Wrong2"
+        Button4.text = "Wrong3"
     }
 
-    fun goBackHome(view: View) {
-        setContentView(R.layout.activity_main)
-    }
+    fun goToCamera(view: View) {
+        setContentView(R.layout.fragment_camera)
 
+        ImageView = findViewById(R.id.imageSave) //Grabs the image view with the id
+        val takePicture: Button = findViewById(R.id.Camera) //Button for taking a picture
+
+        takePicture.setOnClickListener {//When the button is clicked initiate the camera
+            val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            try {
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+            } catch (e: ActivityNotFoundException) {
+                Toast.makeText(this, "Error: " + e.localizedMessage, Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        fun goBackHome(view: View) {
+            setContentView(R.layout.activity_main)
+        }
+
+    }
 }
--- app/src/main/java/com/example/codequarks/Quiz.kt
+++ app/src/main/java/com/example/codequarks/Quiz.kt
@@ -15,29 +15,15 @@ import com.google.firebase.database.getValue
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [Quiz.newInstance] factory method to
- * create an instance of this fragment.
- */
 class Quiz : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
     val database = Firebase.database
     val mainActivity : MainActivity = MainActivity()
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
 
         val quizTextView : TextView = mainActivity.findViewById(R.id.Prompt)
         val quizRef = database.getReference("message").child("Quiz").child("Prompt")
@@ -60,33 +46,7 @@ class Quiz : Fragment() {
         Button2.text = "Wrong"
         Button3.text = "Wrong2"
         Button4.text = "Wrong3"
-    }
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_quiz, container, false)
-    }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment Quiz.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            Quiz().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
     }
 }
